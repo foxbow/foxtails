6f68543fd969a86830b83eb203dabcd2da850131
Björn Weber <foxbow@web.de>
2014-03-06 08:26:43 +0100

    no more PHP code inside HTML pull cookie settings into own file for easier embedding

--- cookies.php
+++ cookies.php
@@ -7,7 +7,8 @@
 		else $first=0;
 		$result .= $part;    	
     }
-    setcookie( "foxtail", "$result" );
+    // One month
+    setcookie( "foxtail", "$result", time()+30*60*24*30 );
     $available=$_POST['available'];
   }
   if( isset($_GET['admin']) && ($_GET['admin']=='logoff') ) {
@@ -22,17 +23,3 @@
     }
   }
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
-<HTML>
-<HEAD>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
-	<link rel="stylesheet" type="text/css" href="foxtails.css" >
-	<TITLE>Cocktails</TITLE>
-</HEAD>
-<BODY>
-<?PHP 
-	require_once( "foxtails.php" ); 
-?>
-</BODY>
-</HTML>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?PHP 
+	require_once( "cookies.php" ); 
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+<HTML>
+<HEAD>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
+	<link rel="stylesheet" type="text/css" href="foxtails.css" >
+	<TITLE>Cocktails</TITLE>
+</HEAD>
+<BODY>
+<?PHP 
+	require_once( "foxtails.php" ); 
+?>
+</BODY>
+</HTML>
